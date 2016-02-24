f3fe7fddd1a8c286fec2185da3d686f1cb7b0a13
Giancarlo Marcelli <gmarcelli83@gmail.com>
2016-02-24 16:17:38 -0500

    added variable types

---
+++ explanations.js
@@ -0,0 +1,13 @@
+var myFavColor = "blue";
+var myFavColors = ['blue', 'red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName : "Giancarlo",
+	lastName : "Marcelli",
+	favColors :  ['blue', 'red', 'green'],
+	yearsAlive : 32,
+	isMale : true
+};
+
+
